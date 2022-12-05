5ad481ebe03fb94b6c8149fc9a2f8222a9944c90
saltanat <saltanatnematillakyzy6@gmail.com>
2022-12-05 20:05:57 +0600

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try{
+            Scanner scanner = new Scanner(System.in);
+            double length = Double.parseDouble(scanner.nextLine());
+            double width = Double.parseDouble(scanner.nextLine());
+            double height = Double.parseDouble(scanner.nextLine());
+            Parallelepiped parallelepiped = new Parallelepiped(length,width,height);
+            System.out.println(parallelepiped);
+            Parallelepiped.getArea(parallelepiped);
+            Parallelepiped.getVolume(parallelepiped);
+        } catch (NumberFormatException e){
+            System.err.println("Tamga beruugo bolboit!");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/Parallelepiped.java
+++ src/Parallelepiped.java
@@ -1,2 +1,60 @@
-package PACKAGE_NAME;public class Parallelepiped {
+public class Parallelepiped {
+    private double length,width,height;
+
+    public Parallelepiped(double length, double width, double height) throws Exception {
+        checkParameters(length,width,height);
+    }
+
+    private void checkParameters(double length,double width,double height) throws Exception {
+        if((length < 0 || length > 20) || (width < 0 || width > 20) || (height < 0 || height > 20))
+            throw new Exception("Parameters must be less than 20 and greater than 0.");
+        else {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "Parallelepiped{" +
+                "length=" + length +
+                ", width=" + width +
+                ", height=" + height +
+                '}';
+    }
+
+    public static void getArea(Parallelepiped parallelepiped){
+        double result = 2 * ((parallelepiped.getLength() * parallelepiped.getHeight()) + (parallelepiped.getLength() * parallelepiped.getWidth()) + (parallelepiped.getHeight() * parallelepiped.getWidth()));
+        System.out.println("Area of Parallelepiped is : " + result);
+    }
+
+    public static void getVolume(Parallelepiped parallelepiped){
+        double result = (parallelepiped.getLength() * parallelepiped.getHeight() * parallelepiped.getWidth());
+        System.out.println("Volume os Parallelepiped is : " + result);
+    }
 }
